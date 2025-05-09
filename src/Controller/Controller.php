620c67205babc63b67b72e0d8295b6bdc5d0c66c
Nvr <nvr312003@gmail.com>
2025-05-09 10:00:11 +0200

    primera version estable

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -1,6 +1,12 @@
 <?php
 namespace App\Controller;
 
+use App\Controller\AppController;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
 class Controller extends AbstractController {
 
     #[Route('/app', name: 'app', methods: ['GET'])]
