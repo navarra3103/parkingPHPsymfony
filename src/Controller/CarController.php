6bfb0719a9a4ff5b3509a28d3a668b82e2570bf1
Nvr <nvr312003@gmail.com>
2025-05-09 11:09:18 +0200

    reparación de error formulario

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -7,6 +7,8 @@ use App\Entity\Coche;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class CarController extends AbstractController
 {
