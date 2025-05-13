a66f011080e6058039803fd984effe21c879b747
Charlygoku <152430771+Charlygoku@users.noreply.github.com>
2025-05-13 09:01:18 +0200

    se ven las matricula

--- src/Controller/ShowParkingController.php
+++ src/Controller/ShowParkingController.php
@@ -2,23 +2,44 @@
 
 namespace App\Controller;
 
-use App\Repository\PlazaRepository;
-use App\Repository\TipoRepository;
+
+use App\Entity\Plaza;
+use App\Entity\Estado;
+use App\Entity\Visita;
+use App\Entity\Tipo;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class ShowParkingController extends AbstractController
 {
     #[Route('/ShowParking', name: 'app_show_parking')]
-    public function index(PlazaRepository $plazaRepository , TipoRepository $TipoRepository): Response
-    {
-        $parkings = $plazaRepository->findAll();
-        $type = $TipoRepository->findAll();
+    public function showParking(EntityManagerInterface $em): Response
+{
+    $plazas = $em->getRepository(Plaza::class)->findAll();
 
-        return $this->render('show_parking/index.html.twig', [
-            'parkings' => $parkings,
-            'types' => $type,
-        ]);
+    // Traer visitas activas, opcionalmente por estado
+    $visitas = $em->getRepository(Visita::class)->findBy([
+        'estado' => $em->getRepository(Estado::class)->findOneBy(['Nombre' => 'Recepcionado']) // o tu lógica
+    ]);
+
+    // Crear un mapa [idPlaza => matricula]
+    $mapaVisitas = [];
+    foreach ($visitas as $visita) {
+        $plaza = $visita->getPlaza();
+        $coche = $visita->getCoche();
+        if ($plaza && $coche) {
+            $mapaVisitas[$plaza->getIdPlaza()] = $coche->getMatricula();
+        }
     }
+
+    return $this->render('show_parking/index.html.twig', [
+        'parkings' => $plazas,
+        'matriculas' => $mapaVisitas,
+        'types' => $em->getRepository(Tipo::class)->findAll(),
+    ]);
+}
+
 }
