08cf668937007e6629d8275f1bb4b07ffa66103e
Charlygoku <carlosagvad@gmail.com>
2025-05-31 02:49:34 +0200

    fix: actualizar requisitos de complejidad de contraseña en el formulario de registro y en el script de validación

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -48,8 +48,8 @@ class RegistrationFormType extends AbstractType
                         ]),
                         // Opcional: Añadir requisitos de complejidad a la contraseña
                         new Regex([
-                            'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/',
-                            'message' => 'La contraseña debe contener al menos una mayúscula, una minúscula, un número y un carácter especial (@$!%*?&).',
+                            'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_\-+=])[A-Za-z\d!@#$%^&*()_\-+=]{8,}$/',
+                            'message' => 'La contraseña debe tener al menos 8 caracteres, incluyendo una letra mayúscula, una minúscula, un número y un símbolo especial (como @, #, $, %, etc.).',
                         ]),
                     ],
                     'label' => 'Contraseña',
